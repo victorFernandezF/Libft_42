398bdb269054f272503e1b610b013558dba8dc3d
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-03-20 10:52:43 +0100

    Docs: add comments to functions

--- str_functions/ft_putstr_fd.c
+++ str_functions/ft_putstr_fd.c
@@ -6,12 +6,21 @@
 /*   By: victofer <victofer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 19:08:56 by victofer          #+#    #+#             */
-/*   Updated: 2023/03/10 11:51:48 by victofer         ###   ########.fr       */
+/*   Updated: 2023/03/20 10:45:02 by victofer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+/*
+ * Function: ft_putstr_fd
+ * ----------------------------
+ *   Writes a string in the given file descriptor.
+ *
+ *   c: char to be writen. 
+ *   fd: file descriptor.
+ *  
+ */
 void	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
